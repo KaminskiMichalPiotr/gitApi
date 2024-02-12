c5669481a264d438ac1ce800403320380fb787c7
TheRudno <therudno@gmail.com>
2024-02-12 20:37:27 +0100

    removed unused import

--- src/main/java/com/example/gitapi/GitApiApplication.java
+++ src/main/java/com/example/gitapi/GitApiApplication.java
@@ -1,6 +1,5 @@
 package com.example.gitapi;
 
-import com.example.gitapi.model.GitApiRepositoryResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
